b3ab14445adeb74917a1b8c737de6528b351685a
Jionghu2022 <100700519+Jionghu2022@users.noreply.github.com>
2022-03-07 18:29:28 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -3,7 +3,12 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     targetImage.addEventListener("click", function () {
 
-        targetImage.classList.remove("small");
+        
+        if (targetImage.classList.contains("small")) {
+            targetImage.classList.remove("small");
+} else {
+            targetImage.classList.add("small");
+}
 
     });
 
